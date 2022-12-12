43a09f1b6e64acddabdcde5ad615e74e21eefd3a
KiLien <kilienazure@gmail.com>
2022-12-12 15:53:01 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import inquirer from 'inquirer';
+import { PROMPT_LIST, CUSTOM_TEMPLATE, IPromptOption } from './assets/inquirer';
+import { handlePromptMethod } from './handle';
+import { createCommand } from './command';
+
+function entry() {
+  inquirer.prompt<IPromptOption>(PROMPT_LIST).then(async (answer) => {
+    const { templateName } = answer;
+    let params = answer;
+    if (templateName === 'custom') {
+      const customParam = await inquirer.prompt(CUSTOM_TEMPLATE);
+      params = Object.assign(params, customParam);
+    }
+    handlePromptMethod(params);
+  });
+}
+
+const cmd = createCommand();
+entry();
