383772dc276b7884bfb380b983a52435c4cdc9d1
KiLien <kilienazure@gmail.com>
2022-12-13 15:07:57 +0800

    发布版本

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
+import figlet from 'figlet';
 import inquirer from 'inquirer';
 import { PROMPT_LIST, CUSTOM_TEMPLATE, IPromptOption } from './assets/inquirer';
 import { handlePromptMethod } from './handle';
 import { createCommand } from './command';
 
 function entry() {
-  inquirer.prompt<IPromptOption>(PROMPT_LIST).then(async (answer) => {
+  const result = figlet.textSync('Dapp-cli', {
+    font: 'Speed',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 80,
+    whitespaceBreak: true
+  });
+  console.log('result...', result);
+  
+  inquirer.prompt<IPromptOption>(PROMPT_LIST).then(async answer => {
     const { templateName } = answer;
     let params = answer;
     if (templateName === 'custom') {
